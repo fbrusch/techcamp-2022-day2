5d9837069e84d26b063ede6ef6c9799d2f4132e2
Francesco Bruschi <fbrusch@gmail.com>
2022-06-14 10:21:37 +0000

    detect being on target

--- main.py
+++ main.py
@@ -9,26 +9,38 @@ screen = pygame.display.set_mode((300,300))
 # state variables
 x = 10
 y = 10
-vx = 1
-vy = 2
+vx = 0
+vy = 0
 
 # target state
 target_x = 100
 target_y = 150
+box_on_target = False
+
+
+
+
+def is_point_in_rect(x, y, rx, ry, rw, rh):
+  return rx < x < rx+rw and ry < y < ry+rh
 
 def render():
   pygame.draw.rect(screen, (100,100,100),   (0,0,300,300))
 
   pygame.draw.rect(screen, (0,255,0), (x, y, 100, 100))
-  pygame.draw.rect(screen, (255,0,0), (target_x,
+  if box_on_target == True:
+    pygame.draw.rect(screen, (255,0,0), (target_x,
                                        target_y, 
                                        30, 30))
+  else:
+    pygame.draw.rect(screen, (0,0,255), (target_x,
+                                     target_y, 
+                                     30, 30))
 
   
   pygame.display.flip()
 
 def controller():
-  global x, y, vx, vy
+  global x, y, vx, vy, target_x, target_y, box_on_target
   pygame.event.get()
   keys = pygame.key.get_pressed()
   if keys[pygame.K_UP]:
@@ -39,7 +51,18 @@ def controller():
     vx -= 0.1
   if keys[pygame.K_RIGHT]:
     vx += 0.1
+
+  if is_point_in_rect(target_x, target_y,
+                     x, y, 100, 100) and \
+     is_point_in_rect(target_x + 30, target_y + 30,
+                     x, y, 100, 100):
+    box_on_target = True
+  else:
+    box_on_target = False
+
   
+  
+    
   x = x + vx
   y = y + vy
   if x + 100 > 300: 
